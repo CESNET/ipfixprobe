73642c0ac28a5342be2479a4f4511e345eb2ddce
Jiri Havranek <havranek@cesnet.cz>
2020-12-01 14:55:38 +0100

    idpcontent: fixed packet payload content export when using flush

--- idpcontentplugin.h
+++ idpcontentplugin.h
@@ -115,9 +115,8 @@ class IDPCONTENTPlugin : public FlowCachePlugin
 public:
    IDPCONTENTPlugin(const options_t &module_options);
    IDPCONTENTPlugin(const options_t &module_options, vector<plugin_opt> plugin_options);
-   int pre_create(Packet &pkt);
    int post_create(Flow &rec, const Packet &pkt);
-   int pre_update(Flow &rec, Packet &pkt);
+   int post_update(Flow &rec, Packet &pkt);
    const char **get_ipfix_string();
    string get_unirec_field_string();
    void update_record(RecordExtIDPCONTENT *pstats_data, const Packet &pkt);
