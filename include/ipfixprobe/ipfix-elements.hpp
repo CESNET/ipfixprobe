a85fdcecf7898e6645c02c1a676e9d0991ed93c5
Roman Vrana <ivrana@fit.vutbr.cz>
2022-05-18 13:10:07 +0200

    Added InterfaceIn for Flexprobe data
    Default input interface item is sourced from a field that doesn't match the information needed by Flexprobe.

--- include/ipfixprobe/ipfix-elements.hpp
+++ include/ipfixprobe/ipfix-elements.hpp
@@ -264,6 +264,7 @@ namespace ipxp {
 
 #ifdef WITH_FLEXPROBE
 #define FX_FRAME_SIGNATURE(F)         F(5715,   1010,  18,   nullptr)
+#define FX_INPUT_INTERFACE(F)         F(5715,   1015,   1,   nullptr)
 #define FX_TCP_TRACKING(F)            F(5715,   1020,   1,   nullptr)
 #endif
 
@@ -498,7 +499,7 @@ namespace ipxp {
    F(OSQUERY_SYSTEM_HOSTNAME)
 
 #ifdef WITH_FLEXPROBE
-#define IPFIX_FLEXPROBE_DATA_TEMPLATE(F) F(FX_FRAME_SIGNATURE)
+#define IPFIX_FLEXPROBE_DATA_TEMPLATE(F) F(FX_FRAME_SIGNATURE) F(FX_INPUT_INTERFACE)
 #define IPFIX_FLEXPROBE_TCP_TEMPLATE(F) F(FX_TCP_TRACKING)
 #define IPFIX_FLEXPROBE_ENCR_TEMPLATE(F)
 #else
