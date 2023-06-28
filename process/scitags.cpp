448afd3f53f9ccacc29a14fa3d72da46992e2eed
Karel Hynek <hynekkar@fit.cvut.cz>
2023-06-28 11:50:30 +0200

    SciTag export: First working implementation of plugin

--- process/scitags.cpp
+++ process/scitags.cpp
@@ -36,55 +36,96 @@ int RecordExtSCITAGS::REGISTERED_ID = -1;
 
 __attribute__((constructor)) static void register_this_plugin()
 {
-   static PluginRecord rec = PluginRecord("scitags", [](){return new SCITAGSPlugin();});
-   register_plugin(&rec);
-   RecordExtSCITAGS::REGISTERED_ID = register_extension();
+    static PluginRecord rec = PluginRecord("scitags", []() { return new SCITAGSPlugin(); });
+    register_plugin(&rec);
+    RecordExtSCITAGS::REGISTERED_ID = register_extension();
 }
 
-SCITAGSPlugin::SCITAGSPlugin()
+SCITAGSPlugin::SCITAGSPlugin() {}
+
+SCITAGSPlugin::~SCITAGSPlugin() {}
+
+void SCITAGSPlugin::init(const char* params) {}
+
+void SCITAGSPlugin::close() {}
+
+ProcessPlugin* SCITAGSPlugin::copy()
 {
+    return new SCITAGSPlugin(*this);
 }
 
-SCITAGSPlugin::~SCITAGSPlugin()
+void SCITAGSPlugin::update_record(RecordExtSCITAGS* record, const Packet& pkt)
 {
+    if (record->flow_label_set == false) {
+        record->flow_label = pkt.ipv6_flowlabel;
+        record->flow_label_set = true;
+        return;
+    }
+
+    if (ntohl(pkt.ipv6_flowlabel) != record->flow_label) {
+        // non constant value across the flow, set to 0
+        record->non_constant_flow_label = true;
+    }
+
+    return;
 }
 
-void SCITAGSPlugin::init(const char *params)
+int SCITAGSPlugin::post_create(Flow& rec, const Packet& pkt)
 {
+    if (pkt.ip_version != 6) {
+        return 0;
+    }
+    RecordExtSCITAGS* data = new RecordExtSCITAGS();
+    if (data == nullptr) {
+        return 1;
+    }
+    rec.add_extension(data);
+
+    update_record(data, pkt);
+    return 0;
 }
 
-void SCITAGSPlugin::close()
+int SCITAGSPlugin::post_update(Flow& rec, const Packet& pkt)
 {
+    if (pkt.ip_version != 6) {
+        return 0;
+    }
+    RecordExtSCITAGS* data = (RecordExtSCITAGS*) rec.get_extension(RecordExtSCITAGS::REGISTERED_ID);
+
+    if (data != nullptr && data->non_constant_flow_label == false) {
+        update_record(data, pkt);
+    }
+
+    return 0;
 }
 
-ProcessPlugin *SCITAGSPlugin::copy()
+uint32_t SCITAGSPlugin::bit_reverse(uint32_t value, uint8_t MSB)
 {
-   return new SCITAGSPlugin(*this);
+    uint32_t result = 0;
+    for (int i = 0; i < MSB; i++) {
+        uint8_t bit = (value & (1 << i)) >> i;
+        result |= bit << (MSB - i - 1);
+    }
+    return result;
 }
 
-int SCITAGSPlugin::pre_create(Packet &pkt)
+void SCITAGSPlugin::pre_export(Flow& rec)
 {
-   return 0;
+    RecordExtSCITAGS* record
+        = (RecordExtSCITAGS*) rec.get_extension(RecordExtSCITAGS::REGISTERED_ID);
+    if (record == nullptr) {
+        return;
+    }
+
+    if (record->non_constant_flow_label == true) {
+        rec.remove_extension(RecordExtSCITAGS::REGISTERED_ID);
+        return;
+    }
+    // Experiment identifier is encoded in 9 bits 14-22 (MSB is bit 0)
+    //(bits are in reversed order to allow for possible future adjustments)
+    record->experiment_id = bit_reverse((record->flow_label & 0x0003FE00) >> 9, 9);
+    // Activity identifier is encoded in 6 bits in position 24-29 (MSB is bit 0)
+    record->experiment_activity = (record->flow_label & 0x000000FC) >> 2;
 }
 
-int SCITAGSPlugin::post_create(Flow &rec, const Packet &pkt)
-{
-   return 0;
-}
-
-int SCITAGSPlugin::pre_update(Flow &rec, Packet &pkt)
-{
-   return 0;
-}
-
-int SCITAGSPlugin::post_update(Flow &rec, const Packet &pkt)
-{
-   return 0;
-}
-
-void SCITAGSPlugin::pre_export(Flow &rec)
-{
-}
-
-}
-
+} // namespace ipxp
--- process/scitags.hpp
+++ process/scitags.hpp
@@ -30,6 +30,7 @@
 #define IPXP_PROCESS_SCITAGS_HPP
 
 #include <cstring>
+#include <sstream>
 
 #ifdef WITH_NEMEA
   #include "fields.h"
@@ -55,13 +56,26 @@ UR_FIELDS (
 struct RecordExtSCITAGS : public RecordExt {
    static int REGISTERED_ID;
 
-   RecordExtSCITAGS() : RecordExt(REGISTERED_ID)
+   uint16_t experiment_id;
+   uint8_t  experiment_activity;
+   uint32_t flow_label;
+   bool     flow_label_set;
+   bool     non_constant_flow_label;
+
+   RecordExtSCITAGS() : RecordExt(REGISTERED_ID),
+      experiment_id(0),
+      experiment_activity(0),
+      flow_label(0),
+      flow_label_set(false),
+      non_constant_flow_label(false)
    {
    }
 
 #ifdef WITH_NEMEA
    virtual void fill_unirec(ur_template_t *tmplt, void *record)
    {
+      ur_set(tmplt, record, F_SCITAG_EXPERIMENT_ID, experiment_id);
+      ur_set(tmplt, record, F_SCITAG_EXPERIMENT_ACTIVITY, experiment_activity);
    }
 
    const char *get_unirec_tmplt() const
@@ -72,7 +86,12 @@ struct RecordExtSCITAGS : public RecordExt {
 
    virtual int fill_ipfix(uint8_t *buffer, int size)
    {
-      return 0;
+      if ((size_t)size < (sizeof(uint16_t) + sizeof(uint8_t))) {
+         return -1;
+      }
+      *(uint16_t*)buffer = htons(experiment_id);
+      *(buffer + 2) = experiment_activity;
+      return sizeof(uint16_t) + sizeof(uint8_t);
    }
 
    const char **get_ipfix_tmplt() const
@@ -82,6 +101,14 @@ struct RecordExtSCITAGS : public RecordExt {
          NULL
       };
       return ipfix_template;
+   }
+
+   std::string get_text() const
+   {
+      std::ostringstream out;
+      out << "experimentID=" << (uint32_t)experiment_id << ",";
+      out << "experimentActivity=" << (uint32_t)experiment_activity << ",";
+      return out.str();
    }
 };
 
@@ -100,10 +127,10 @@ public:
    RecordExt *get_ext() const { return new RecordExtSCITAGS(); }
    ProcessPlugin *copy();
 
-   int pre_create(Packet &pkt);
    int post_create(Flow &rec, const Packet &pkt);
-   int pre_update(Flow &rec, Packet &pkt);
    int post_update(Flow &rec, const Packet &pkt);
+   void update_record(RecordExtSCITAGS *record, const Packet &pkt);
+   static uint32_t bit_reverse(uint32_t value, uint8_t MSB);
    void pre_export(Flow &rec);
 };
 
