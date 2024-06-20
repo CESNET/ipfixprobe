CESNET/ipfixprobe

commit dd3cd88f2e2fb7509c503815eefe4fe36412a2da

dpdk - configure MTU size in dpdkDevice

input/dpdk/dpdkDevice.cpp
@@ -39,7 +39,8 @@ DpdkDevice::DpdkDevice(
 	uint16_t portID,
 	uint16_t rxQueueCount,
 	uint16_t memPoolSize,
-	uint16_t mbufsCount)
+	uint16_t mbufsCount,
+	uint16_t mtuSize)
 	: m_portID(portID)
 	, m_rxQueueCount(rxQueueCount)
 	, m_txQueueCount(0)
@@ -47,6 +48,7 @@ DpdkDevice::DpdkDevice(
 	, m_isNfbDpdkDriver(false)
 	, m_supportedRSS(false)
 	, m_supportedHWTimestamp(false)
+	, m_mtuSize(mtuSize)
 {
 	validatePort();
 	recognizeDriver();
@@ -137,9 +139,9 @@ rte_eth_conf DpdkDevice::createPortConfig()
 	}
 
 #if RTE_VERSION >= RTE_VERSION_NUM(21, 11, 0, 0)
-	rte_eth_conf portConfig {.rxmode = {.mtu = RTE_ETHER_MAX_LEN}};
+	rte_eth_conf portConfig {.rxmode = {.mtu = m_mtuSize}};
 #else
-	rte_eth_conf portConfig {.rxmode = {.max_rx_pkt_len = RTE_ETHER_MAX_LEN}};
+	rte_eth_conf portConfig {.rxmode = {.max_rx_pkt_len = m_mtuSize}};
 #endif
 
 	if (m_supportedRSS) {
@@ -172,7 +174,7 @@ void DpdkDevice::initMemPools(uint16_t memPoolSize)
 			memPoolSize,
 			MEMPOOL_CACHE_SIZE,
 			0,
-			RTE_MBUF_DEFAULT_BUF_SIZE,
+			std::max(m_mtuSize, (uint16_t)RTE_MBUF_DEFAULT_DATAROOM) + RTE_PKTMBUF_HEADROOM,
 			rte_eth_dev_socket_id(m_portID));
 		if (!memPool) {
 			throw PluginError(

input/dpdk.cpp
@@ -155,12 +155,13 @@ void DpdkCore::configure(const char* params)
     uint16_t mempoolSize = parser.pkt_mempool_size();
     uint16_t rxQueueCount = parser.rx_queues();
     m_mBufsCount = parser.pkt_buffer_size();
+    uint16_t mtuSize = parser.mtu_size();
 
     configureEal(parser.eal_params());
 
     m_dpdkDevices.reserve(parser.port_numbers().size());
     for (auto portID :  parser.port_numbers()) {
-        m_dpdkDevices.emplace_back(portID, rxQueueCount, mempoolSize, m_mBufsCount);
+        m_dpdkDevices.emplace_back(portID, rxQueueCount, mempoolSize, m_mBufsCount, mtuSize);
     }
 
     isConfigured = true;

input/dpdk/dpdkDevice.hpp
@@ -45,8 +45,9 @@ public:
 	 * @param rxQueueCount The number of receive queues to be configured.
 	 * @param memPoolSize The size of the memory pool for packet buffers.
 	 * @param mbufsCount The number of mbufs (packet buffers) to be allocated.
+	 * @param mtuSize Maximum transmission unit of input interface.
 	 */
-	DpdkDevice(uint16_t portID, uint16_t rxQueueCount, uint16_t memPoolSize, uint16_t mbufsCount);
+	DpdkDevice(uint16_t portID, uint16_t rxQueueCount, uint16_t memPoolSize, uint16_t mbufsCount, uint16_t mtuSize);
 
 	/**
 	 * @brief Receives packets from the specified receive queue of the DPDK device.
@@ -92,6 +93,7 @@ private:
 	bool m_supportedHWTimestamp;
 	int m_rxTimestampOffset;
 	int m_rxTimestampDynflag;
+	uint16_t m_mtuSize;
 
 };
 

commit b04ad8aa0ae8c2f11b0d378dd998032f52537f79

dpdk - fix mbuf dataroom size MTU is increased by eth header len.
Fixes: dd3cd88f2e2fb7509c503815eefe4fe36412a2da

input/dpdk/dpdkDevice.cpp
@@ -163,6 +163,8 @@ rte_eth_conf DpdkDevice::createPortConfig()
 void DpdkDevice::initMemPools(uint16_t memPoolSize)
 {
 	constexpr int MEMPOOL_CACHE_SIZE = 256;
+	constexpr int VLAN_HDR_LEN = 4;
+	const int max_packet_size = m_mtuSize + RTE_ETHER_HDR_LEN + VLAN_HDR_LEN;
 
 	m_memPools.reserve(m_rxQueueCount);
 
@@ -174,7 +176,7 @@ void DpdkDevice::initMemPools(uint16_t memPoolSize)
 			memPoolSize,
 			MEMPOOL_CACHE_SIZE,
 			0,
-			std::max(m_mtuSize, (uint16_t)RTE_MBUF_DEFAULT_DATAROOM) + RTE_PKTMBUF_HEADROOM,
+			std::max(max_packet_size, RTE_MBUF_DEFAULT_DATAROOM) + RTE_PKTMBUF_HEADROOM,
 			rte_eth_dev_socket_id(m_portID));
 		if (!memPool) {
 			throw PluginError(