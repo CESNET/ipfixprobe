888dbfc4f3bea4261fdfb4af30c6d4629e344104
Damir Zainullin <zaidamilda@gmail.com>
2024-10-01 23:00:04 +0200

    Adjust PHISTS plugin

--- process/phists.hpp
+++ process/phists.hpp
@@ -193,8 +193,8 @@ public:
    RecordExt *get_ext() const { return new RecordExtPHISTS(); }
    ProcessPlugin *copy();
 
-   int post_create(Flow &rec, const Packet &pkt);
-   int post_update(Flow &rec, const Packet &pkt);
+   ProcessPlugin::FlowAction post_create(Flow &rec, const Packet &pkt);
+   ProcessPlugin::FlowAction post_update(Flow &rec, const Packet &pkt);
 
 private:
    bool use_zeros;
