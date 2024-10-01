00067110429e06224f7e436553cc8a01b706313c
Damir Zainullin <zaidamilda@gmail.com>
2024-10-01 23:03:23 +0200

    Adjust WG plugin

--- process/wg.hpp
+++ process/wg.hpp
@@ -158,8 +158,8 @@ public:
    RecordExt *get_ext() const { return new RecordExtWG(); }
    ProcessPlugin *copy();
 
-   int post_create(Flow &rec, const Packet &pkt);
-   int pre_update(Flow &rec, Packet &pkt);
+   ProcessPlugin::FlowAction post_create(Flow &rec, const Packet &pkt);
+   ProcessPlugin::FlowAction pre_update(Flow &rec, Packet &pkt);
    void pre_export(Flow &rec);
    void finish(bool print_stats);
 
