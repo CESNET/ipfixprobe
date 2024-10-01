c278cd0f22a1f522b46a8cf5f3b2eb23268d62ca
Damir Zainullin <zaidamilda@gmail.com>
2024-10-01 23:01:35 +0200

    Adjust SMTP plugin

--- process/smtp.hpp
+++ process/smtp.hpp
@@ -241,8 +241,8 @@ public:
    RecordExt *get_ext() const { return new RecordExtSMTP(); }
    ProcessPlugin *copy();
 
-   int post_create(Flow &rec, const Packet &pkt);
-   int pre_update(Flow &rec, Packet &pkt);
+   ProcessPlugin::FlowAction post_create(Flow &rec, const Packet &pkt);
+   ProcessPlugin::FlowAction pre_update(Flow &rec, Packet &pkt);
    void finish(bool print_stats);
 
    bool smtp_keyword(const char *data);
