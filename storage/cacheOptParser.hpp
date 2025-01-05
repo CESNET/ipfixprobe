dd46483a2e881bdedee8fb60842692f8d917019f
Damir Zainullin <zaidamilda@gmail.com>
2025-01-05 23:31:04 +0100

    cache opt parser refactor

--- storage/cacheOptParser.hpp
+++ storage/cacheOptParser.hpp
@@ -41,9 +41,6 @@ public:
    bool m_enable_fragmentation_cache;
    std::size_t m_frag_cache_size;
    time_t m_frag_cache_timeout;
-   #ifdef WITH_CTT
-   std::string m_dev;
-   #endif /* WITH_CTT */
 
    CacheOptParser();
 };
