44c0c8d5a33ad70bc2d694f8b714fab60aba1e7f
Damir Zainullin <zaidamilda@gmail.com>
2025-01-06 08:19:03 +0100

    ++ cache

--- storage/cache.cpp
+++ storage/cache.cpp
@@ -184,6 +184,7 @@ void NHTFlowCache::finish()
          if (m_flow_table[i]->is_in_ctt && !m_flow_table[i]->is_waiting_for_export) {
             send_export_request_to_ctt(m_flow_table[i]->m_flow.flow_hash_ctt);
             m_hashes_in_ctt[m_flow_table[i]->m_flow.flow_hash_ctt] -= 1;
+            std::cout << m_hashes_in_ctt[m_flow_table[i]->m_flow.flow_hash_ctt] << std::endl;
             if (m_hashes_in_ctt[m_flow_table[i]->m_flow.flow_hash_ctt] < 0)
             {
                throw "bad ctt record";
