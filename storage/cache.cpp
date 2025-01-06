3c770e724826636c9f9341a2a6a4916523fc819c
Damir Zainullin <zaidamilda@gmail.com>
2025-01-06 17:45:13 +0100

    ++ cachr

--- storage/cache.cpp
+++ storage/cache.cpp
@@ -286,6 +286,7 @@ void NHTFlowCache::create_record(const Packet& packet, size_t flow_index, size_t
    m_flow_table[flow_index]->m_flow.flow_hash_ctt = packet.cttmeta.flow_hash;
    if (only_metadata_required(m_flow_table[flow_index]->m_flow)) {
       m_hashes_in_ctt[m_flow_table[flow_index]->m_flow.flow_hash_ctt]++;
+      auto x = m_hashes_in_ctt[m_flow_table[flow_index]->m_flow.flow_hash_ctt];
       m_ctt_controller.create_record(m_flow_table[flow_index]->m_flow.flow_hash_ctt, m_flow_table[flow_index]->m_flow.time_first);
       m_flow_table[flow_index]->is_in_ctt = true;
    }
