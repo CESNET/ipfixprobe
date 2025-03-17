546e56409d019b67f97c6f5c2c2edfdbd6171d1e
Pavel Siska <xsiska12@ta-devel.liberouter.org>
2025-03-17 17:19:23 +0100

    ipfixprobe - introduce storage CACHE plugin

--- src/plugins/storage/cache/src/xxhash.h
+++ src/plugins/storage/cache/src/xxhash.h
@@ -5030,10 +5030,14 @@ XXH3_mergeAccs(const xxh_u64* XXH_RESTRICT acc, const xxh_u8* XXH_RESTRICT secre
 }
 
 #define XXH3_INIT_ACC                                                                              \
-	{                                                                                              \
-		XXH_PRIME32_3, XXH_PRIME64_1, XXH_PRIME64_2, XXH_PRIME64_3, XXH_PRIME64_4, XXH_PRIME32_2,  \
-			XXH_PRIME64_5, XXH_PRIME32_1                                                           \
-	}
+	{XXH_PRIME32_3,                                                                                \
+	 XXH_PRIME64_1,                                                                                \
+	 XXH_PRIME64_2,                                                                                \
+	 XXH_PRIME64_3,                                                                                \
+	 XXH_PRIME64_4,                                                                                \
+	 XXH_PRIME32_2,                                                                                \
+	 XXH_PRIME64_5,                                                                                \
+	 XXH_PRIME32_1}
 
 XXH_FORCE_INLINE XXH64_hash_t XXH3_hashLong_64b_internal(
 	const void* XXH_RESTRICT input,
