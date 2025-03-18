e98641c9761e41407418ec25132c35a2294bff6e
Pavel Siska <xsiska12@ta-devel.liberouter.org>
2025-03-18 20:11:51 +0100

    ipfixprobe - introduce input DPDK plugin

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -8,3 +8,7 @@ find_package(Unwind REQUIRED)
 if (ENABLE_INPUT_PCAP)
 	pkg_check_modules(PCAP REQUIRED libpcap)
 endif()
+	
+if (ENABLE_INPUT_DPDK)
+	pkg_check_modules(DPDK REQUIRED libdpdk)
+endif()
