46fe8752b47c873579c2b5eaf9e21d9f4d2c57f2
Pavel Siska <xsiska12@ta-devel.liberouter.org>
2025-03-19 11:30:01 +0100

    ipfixprobe - introduce output IPFIX plugin

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -4,6 +4,7 @@ find_package(PkgConfig REQUIRED)
 find_package(Threads REQUIRED)
 find_package(Atomic REQUIRED)
 find_package(Unwind REQUIRED)
+find_package(LZ4 REQUIRED)
 
 if (ENABLE_INPUT_PCAP)
 	pkg_check_modules(PCAP REQUIRED libpcap)
