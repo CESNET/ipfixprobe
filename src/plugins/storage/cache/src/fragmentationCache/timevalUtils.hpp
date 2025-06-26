41c57b370d2bd17e50268bb2a635357f2362b37b
Damir Zainullin <zainudam@laurot.liberouter.org>
2025-06-26 10:30:47 +0200

    CTT - Update timeval utils functions

--- src/plugins/storage/cache/src/fragmentationCache/timevalUtils.hpp
+++ src/plugins/storage/cache/src/fragmentationCache/timevalUtils.hpp
@@ -22,13 +22,13 @@
  *    software without specific prior written permission.
  */
 
-#include <sys/time.h>
+ #include <sys/time.h>
 
 #pragma once
 
 namespace ipxp {
 
-struct timeval operator+(const struct timeval& a, const struct timeval& b) noexcept
+inline struct timeval operator+(const struct timeval& a, const struct timeval& b) noexcept
 {
 	constexpr time_t USEC_IN_SEC = 1000000;
 
@@ -42,11 +42,33 @@ struct timeval operator+(const struct timeval& a, const struct timeval& b) noexc
 	return result;
 }
 
-bool operator>(const struct timeval& a, const struct timeval& b) noexcept
+inline struct timeval operator-(const struct timeval& a, const struct timeval& b) noexcept
+{
+	struct timeval result;
+	result.tv_sec = a.tv_sec - b.tv_sec;
+	result.tv_usec = a.tv_usec - b.tv_usec;
+	if (result.tv_usec < 0) {
+		result.tv_sec--;
+		result.tv_usec += 1000000;
+	}
+	return result;
+}
+
+inline bool operator>(const struct timeval& a, const struct timeval& b) noexcept
 {
 	if (a.tv_sec == b.tv_sec)
 		return a.tv_usec > b.tv_usec;
 	return a.tv_sec > b.tv_sec;
+}
+
+inline bool operator==(const struct timeval& a, const struct timeval& b) noexcept
+{
+	return a.tv_sec == b.tv_sec && a.tv_usec == b.tv_usec;
+}
+
+inline bool operator>=(const struct timeval& a, const struct timeval& b) noexcept
+{
+	return a > b || a == b;
 }
 
 } // namespace ipxp
