4e57642068b11d20d3f1c977cd170fb2c7d03d75
Damir Zainullin <zainudam@laurot.liberouter.org>
2025-06-26 10:56:12 +0200

    CTT - Update VLAN process plugin

--- src/plugins/process/vlan/src/vlan.hpp
+++ src/plugins/process/vlan/src/vlan.hpp
@@ -92,7 +92,7 @@ public:
 	RecordExt* get_ext() const { return new RecordExtVLAN(m_pluginID); }
 	ProcessPlugin* copy();
 
-	int post_create(Flow& rec, const Packet& pkt);
+	ProcessPlugin::FlowAction post_create(Flow& rec, const Packet& pkt);
 };
 
 } // namespace ipxp
