4880cb9dfcc39d9597c2675e9fd1ab8aa56ab80f
Damir Zainullin <zainudam@laurot.liberouter.org>
2025-06-26 10:52:00 +0200

    CTT - Update DNS process plugin

--- src/plugins/process/dns/src/dns.hpp
+++ src/plugins/process/dns/src/dns.hpp
@@ -61,7 +61,9 @@ struct RecordExtDNS : public RecordExt {
 	char data[160];
 	uint16_t psize;
 	uint8_t dns_do;
+	uint16_t parsing_tries;
 
+	static constexpr size_t MAX_PARSING_TRIES = 30;
 	/**
 	 * \brief Constructor.
 	 */
@@ -157,8 +159,8 @@ public:
 	RecordExt* get_ext() const { return new RecordExtDNS(m_pluginID); }
 	ProcessPlugin* copy();
 
-	int post_create(Flow& rec, const Packet& pkt);
-	int post_update(Flow& rec, const Packet& pkt);
+	ProcessPlugin::FlowAction post_create(Flow& rec, const Packet& pkt);
+	ProcessPlugin::FlowAction post_update(Flow& rec, const Packet& pkt);
 	void finish(bool print_stats);
 
 private:
@@ -170,7 +172,7 @@ private:
 	uint32_t data_len; /**< Length of packet payload. */
 
 	bool parse_dns(const char* data, unsigned int payload_len, bool tcp, RecordExtDNS* rec);
-	int add_ext_dns(const char* data, unsigned int payload_len, bool tcp, Flow& rec);
+	ProcessPlugin::FlowAction add_ext_dns(const char* data, unsigned int payload_len, bool tcp, Flow& rec);
 	void process_srv(std::string& str) const;
 	void process_rdata(
 		const char* record_begin,
