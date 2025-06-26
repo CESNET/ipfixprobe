64443129a9ed91d08ee54caf945434eed7b683e2
Damir Zainullin <zainudam@laurot.liberouter.org>
2025-06-26 10:58:40 +0200

    CTT - Update PassiveDNS process plugin

--- src/plugins/process/passiveDns/src/passivedns.cpp
+++ src/plugins/process/passiveDns/src/passivedns.cpp
@@ -102,7 +102,7 @@ ProcessPlugin* PassiveDNSPlugin::copy()
 	return new PassiveDNSPlugin(*this);
 }
 
-int PassiveDNSPlugin::post_create(Flow& rec, const Packet& pkt)
+ProcessPlugin::FlowAction PassiveDNSPlugin::post_create(Flow& rec, const Packet& pkt)
 {
 	if (pkt.src_port == 53) {
 		return add_ext_dns(
@@ -111,11 +111,14 @@ int PassiveDNSPlugin::post_create(Flow& rec, const Packet& pkt)
 			pkt.ip_proto == IPPROTO_TCP,
 			rec);
 	}
+	if (pkt.dst_port == 53) {
+		return ProcessPlugin::FlowAction::GET_ALL_DATA;
+	}
 
-	return 0;
+	return ProcessPlugin::FlowAction::GET_NO_DATA;
 }
 
-int PassiveDNSPlugin::post_update(Flow& rec, const Packet& pkt)
+ProcessPlugin::FlowAction PassiveDNSPlugin::post_update(Flow& rec, const Packet& pkt)
 {
 	if (pkt.src_port == 53) {
 		return add_ext_dns(
@@ -125,7 +128,7 @@ int PassiveDNSPlugin::post_update(Flow& rec, const Packet& pkt)
 			rec);
 	}
 
-	return 0;
+	return ProcessPlugin::FlowAction::GET_ALL_DATA;
 }
 
 void PassiveDNSPlugin::finish(bool print_stats)
@@ -507,14 +510,14 @@ bool PassiveDNSPlugin::process_ptr_record(std::string name, RecordExtPassiveDNS*
  * \param [in] tcp DNS over tcp.
  * \param [out] rec Destination Flow.
  */
-int PassiveDNSPlugin::add_ext_dns(const char* data, unsigned int payload_len, bool tcp, Flow& rec)
+ProcessPlugin::FlowAction PassiveDNSPlugin::add_ext_dns(const char* data, unsigned int payload_len, bool tcp, Flow& rec)
 {
 	RecordExt* tmp = parse_dns(data, payload_len, tcp);
 	if (tmp != nullptr) {
 		rec.add_extension(tmp);
 	}
 
-	return FLOW_FLUSH;
+	return ProcessPlugin::FlowAction::FLUSH;
 }
 
 static const PluginRegistrar<PassiveDNSPlugin, ProcessPluginFactory>
