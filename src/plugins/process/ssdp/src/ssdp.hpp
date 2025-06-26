eba3e223fd08aa215c8135b863a595a05e2429e4
Damir Zainullin <zainudam@laurot.liberouter.org>
2025-06-26 11:00:54 +0200

    CTT - Update SSDP process plugin

--- src/plugins/process/ssdp/src/ssdp.hpp
+++ src/plugins/process/ssdp/src/ssdp.hpp
@@ -154,8 +154,8 @@ public:
 	RecordExt* get_ext() const { return new RecordExtSSDP(m_pluginID); }
 	ProcessPlugin* copy();
 
-	int post_create(Flow& rec, const Packet& pkt);
-	int pre_update(Flow& rec, Packet& pkt);
+	ProcessPlugin::FlowAction post_create(Flow& rec, const Packet& pkt);
+	ProcessPlugin::FlowAction pre_update(Flow& rec, Packet& pkt);
 	void finish(bool print_stats);
 
 	/**
