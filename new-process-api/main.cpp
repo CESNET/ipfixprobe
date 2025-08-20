df178b60489427c9521408088fae317ffcc6aaa7
Pavel Siska <xsiska12@ta-devel.liberouter.org>
2025-08-20 10:33:21 +0200

    main

---
+++ new-process-api/main.cpp
@@ -0,0 +1,40 @@
+#include "fieldManager.hpp"
+#include "flowKey.hpp"
+#include "flowRecordBuilder.hpp"
+#include "icmp.hpp"
+#include "processPluginBuilder.hpp"
+#include "textOutputPlugin.hpp"
+
+#include <iostream>
+
+using namespace ipxp;
+
+int main()
+{
+	FieldManager fieldManager;
+	ProcessPlugins processPlugins(fieldManager);
+
+	processPlugins.addProcessPlugin("TestDummy", "Params");
+
+	processPlugins.disableProcessPlugin("TestDummy");
+	processPlugins.enableProcessPlugin("TestDummy");
+
+	FlowRecordBuilder builder(processPlugins.getEntries(), VlanFlowKeyLayout);
+
+	builder.printLayoutInfo();
+
+	auto flowRecord = builder.build();
+
+	/**
+	processPlugins.forEachPlugin(
+		flowRecord.get(),
+		[&](FlowRecord* flowRecord, ProcessPlugin* plugin, void* pluginContext) {
+			plugin->onInit(*flowRecord, pluginContext, Packet(), PacketFeatures());
+		});
+	*/
+
+	TextOutputPlugin textOutputPlugin(fieldManager, processPlugins.getEntries());
+	textOutputPlugin.processRecord(flowRecord);
+
+	return 0;
+}
