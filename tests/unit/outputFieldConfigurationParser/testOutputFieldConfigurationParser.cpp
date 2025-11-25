baba9a4a628c5ef142d7789e5cd0b011c54100f2
Zainullin Damir <zadamsa@Xavier.>
2025-11-25 11:43:08 +0100

    OutputConfigurationParser - Add tests

---
+++ tests/unit/outputFieldConfigurationParser/testOutputFieldConfigurationParser.cpp
@@ -0,0 +1,190 @@
+/**
+ * @file
+ * @brief Tests for outputFieldConfigurationParser.
+ * @author Damir Zainullin <zaidamilda@gmail.com>
+ * @date 2025
+ *
+ * @copyright Copyright (c) 2025 CESNET, z.s.p.o.
+ */
+
+#include <fstream>
+#include <ranges>
+#include <set>
+#include <string_view>
+#include <vector>
+
+#include <fieldGroup.hpp>
+#include <fieldHandler.hpp>
+#include <fieldHandlersEnum.hpp>
+#include <fieldManager.hpp>
+#include <gtest/gtest.h>
+#include <outputFieldConfigurationParser.hpp>
+
+using namespace ipxp::process;
+using namespace ipxp;
+
+static auto dummyGetter = [](const void*) { return 0; };
+
+enum class QUICFields { QUIC_F1, QUIC_F2, FIELDS_SIZE };
+
+enum class TLSFields { TLS_F1, TLS_F2, FIELDS_SIZE };
+
+void addDummyOutputFields(FieldManager& fieldManager)
+{
+	FieldGroup quicSchema = fieldManager.createFieldGroup("quic");
+	FieldHandlers<QUICFields> quicFieldHandlers;
+
+	quicFieldHandlers.insert(
+		QUICFields::QUIC_F1,
+		quicSchema.addScalarField("QUIC_F1", dummyGetter));
+	quicFieldHandlers.insert(
+		QUICFields::QUIC_F2,
+		quicSchema.addScalarField("QUIC_F2", dummyGetter));
+
+	FieldGroup tlsSchema = fieldManager.createFieldGroup("tls");
+	FieldHandlers<TLSFields> tlsFieldHandlers;
+
+	tlsFieldHandlers.insert(TLSFields::TLS_F1, tlsSchema.addScalarField("TLS_F1", dummyGetter));
+	tlsFieldHandlers.insert(TLSFields::TLS_F2, tlsSchema.addScalarField("TLS_F2", dummyGetter));
+}
+
+struct KeyFields {
+	std::string_view group;
+	std::string_view name;
+
+	constexpr bool operator<(const KeyFields& other) const noexcept
+	{
+		return std::tie(group, name) < std::tie(other.group, other.name);
+	}
+
+	constexpr bool operator==(const KeyFields& other) const noexcept
+	{
+		return std::tie(group, name) == std::tie(other.group, other.name);
+	}
+};
+
+std::set<KeyFields> extractKeyFields(const std::vector<const FieldDescriptor*>& descriptors)
+{
+	return descriptors | std::views::transform([](const FieldDescriptor* desc) {
+			   return KeyFields {desc->getGroup(), desc->getName()};
+		   })
+		| std::ranges::to<std::set>();
+}
+
+std::set<KeyFields> getKeyOutputFieldsFromConfig(std::string_view configPath)
+{
+	FieldManager fieldManager;
+	addDummyOutputFields(fieldManager);
+	return extractKeyFields(
+		OutputFieldParser::getOutputFields(fieldManager.getUniflowForwardFields(), configPath));
+}
+
+TEST(TestOutputFieldConfigurationParser, TestAll1)
+{
+	std::system("pwd > ~/x.txt");
+
+	EXPECT_EQ(
+		getKeyOutputFieldsFromConfig(
+			"../../../../tests/unit/outputFieldConfigurationParser/inputs/all1.cfg"),
+		(std::set<KeyFields> {
+			{"quic", "QUIC_F1"},
+			{"quic", "QUIC_F2"},
+			{"tls", "TLS_F1"},
+			{"tls", "TLS_F2"},
+		}));
+}
+
+TEST(TestOutputFieldConfigurationParser, TestAll2)
+{
+	EXPECT_EQ(
+		getKeyOutputFieldsFromConfig(
+			"../../../../tests/unit/outputFieldConfigurationParser/inputs/all2.cfg"),
+		(std::set<KeyFields> {
+			{"quic", "QUIC_F1"},
+			{"quic", "QUIC_F2"},
+			{"tls", "TLS_F1"},
+			{"tls", "TLS_F2"},
+		}));
+}
+
+TEST(TestOutputFieldConfigurationParser, TestAll3)
+{
+	EXPECT_EQ(
+		getKeyOutputFieldsFromConfig(
+			"../../../../tests/unit/outputFieldConfigurationParser/inputs/all3.cfg"),
+		(std::set<KeyFields> {
+			{"quic", "QUIC_F1"},
+			{"quic", "QUIC_F2"},
+			{"tls", "TLS_F1"},
+			{"tls", "TLS_F2"},
+		}));
+}
+
+TEST(TestOutputFieldConfigurationParser, TestAll4)
+{
+	EXPECT_EQ(
+		getKeyOutputFieldsFromConfig(
+			"../../../../tests/unit/outputFieldConfigurationParser/inputs/all4.cfg"),
+		(std::set<KeyFields> {
+			{"quic", "QUIC_F1"},
+			{"quic", "QUIC_F2"},
+			{"tls", "TLS_F1"},
+			{"tls", "TLS_F2"},
+		}));
+}
+
+TEST(TestOutputFieldConfigurationParser, TestAll5)
+{
+	EXPECT_EQ(
+		getKeyOutputFieldsFromConfig(
+			"../../../../tests/unit/outputFieldConfigurationParser/inputs/all5.cfg"),
+		(std::set<KeyFields> {
+			{"quic", "QUIC_F1"},
+			{"quic", "QUIC_F2"},
+			{"tls", "TLS_F1"},
+			{"tls", "TLS_F2"},
+		}));
+}
+
+TEST(TestOutputFieldConfigurationParser, TestEmpty1)
+{
+	EXPECT_TRUE(getKeyOutputFieldsFromConfig(
+					"../../../../tests/unit/outputFieldConfigurationParser/inputs/empty1.cfg")
+					.empty());
+}
+
+TEST(TestOutputFieldConfigurationParser, TestEmpty2)
+{
+	EXPECT_TRUE(getKeyOutputFieldsFromConfig(
+					"../../../../tests/unit/outputFieldConfigurationParser/inputs/empty2.cfg")
+					.empty());
+}
+
+TEST(TestOutputFieldConfigurationParser, TestEmpty3)
+{
+	EXPECT_TRUE(getKeyOutputFieldsFromConfig(
+					"../../../../tests/unit/outputFieldConfigurationParser/inputs/empty3.cfg")
+					.empty());
+}
+
+TEST(TestOutputFieldConfigurationParser, TestTLS1)
+{
+	EXPECT_EQ(
+		getKeyOutputFieldsFromConfig(
+			"../../../../tests/unit/outputFieldConfigurationParser/inputs/tls1.cfg"),
+		(std::set<KeyFields> {
+			{"tls", "TLS_F1"},
+			{"tls", "TLS_F2"},
+		}));
+}
+
+TEST(TestOutputFieldConfigurationParser, TestTLS2)
+{
+	EXPECT_EQ(
+		getKeyOutputFieldsFromConfig(
+			"../../../../tests/unit/outputFieldConfigurationParser/inputs/tls2.cfg"),
+		(std::set<KeyFields> {
+			{"tls", "TLS_F1"},
+			{"tls", "TLS_F2"},
+		}));
+}
