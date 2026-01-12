ae9d8478ad6da17c9dc1b587abf8c1dfc8f8e647
Zainullin Damir <zadamsa@Xavier.>
2026-01-12 22:19:13 +0100

    Output Storage - Introduce Ring output storage for comparison to original version

---
+++ include/ipfixprobe/outputPlugin/outputStorage/ringOutputStorage.hpp
@@ -0,0 +1,61 @@
+#pragma once
+
+#include "../../ring.h"
+#include "outputStorage.hpp"
+
+#include <atomic>
+#include <condition_variable>
+#include <mutex>
+#include <optional>
+
+#include <boost/container/static_vector.hpp>
+
+namespace ipxp::output {
+
+class RingOutputStorage : public OutputStorage {
+public:
+	explicit RingOutputStorage(const uint8_t writersCount) noexcept
+		: OutputStorage(writersCount)
+		, m_ring(
+			  ipx_ring_init(
+				  static_cast<uint32_t>(ALLOCATION_BUFFER_CAPACITY * 32),
+				  writersCount > 1),
+			  &ipx_ring_destroy)
+	{
+		static_assert(sizeof(ContainerWrapper) == sizeof(void*));
+	}
+
+	void storeContainer(ContainerWrapper container) noexcept override
+	{
+		if (container.empty()) {
+			throw std::runtime_error("Attempt to store empty container");
+		}
+		ipx_ring_push(m_ring.get(), *reinterpret_cast<void**>(&container));
+	}
+
+	std::optional<ReferenceCounterHandler<OutputContainer>>
+	getContainer(const std::size_t readerGroupIndex) noexcept override
+	{
+		if (ipx_ring_cnt(m_ring.get()) == 0) {
+			return std::nullopt;
+		}
+
+		auto pop = ipx_ring_pop(m_ring.get());
+		if (pop == nullptr) {
+			return std::nullopt;
+		}
+		ContainerWrapper& container = *reinterpret_cast<ContainerWrapper*>(&pop);
+		return std::make_optional<ReferenceCounterHandler<OutputContainer>>(
+			getReferenceCounter(container));
+	}
+
+	bool finished(const std::size_t readerGroupIndex) noexcept override
+	{
+		return !writersPresent();
+	}
+
+private:
+	std::unique_ptr<ipx_ring_t, decltype(&ipx_ring_destroy)> m_ring;
+};
+
+} // namespace ipxp::output
