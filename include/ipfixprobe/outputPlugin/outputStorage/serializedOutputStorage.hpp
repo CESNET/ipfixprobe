d01a3bb70dd42d75ff775a4f1e198d2ed86c73da
Zainullin Damir <zadamsa@Xavier.>
2026-01-12 13:39:12 +0100

    Output Storage - Introduce Serialized OutputStorages

---
+++ include/ipfixprobe/outputPlugin/outputStorage/serializedOutputStorage.hpp
@@ -0,0 +1,77 @@
+#pragma once
+
+#include "outputStorage.hpp"
+
+#include <algorithm>
+#include <atomic>
+#include <functional>
+#include <memory>
+#include <mutex>
+#include <optional>
+#include <ranges>
+#include <vector>
+
+namespace ipxp::output {
+
+class SerializedOutputStorage : public OutputStorage {
+public:
+	explicit SerializedOutputStorage(const uint8_t writersCount) noexcept
+		: OutputStorage(writersCount)
+	{
+	}
+
+	std::size_t registerReaderGroup(const uint8_t groupSize) noexcept override
+	{
+		const std::size_t index = OutputStorage::registerReaderGroup(groupSize);
+		std::lock_guard<std::mutex> lock(m_storageMutex);
+		m_readIndex.push_back(m_writeIndex);
+		return index;
+	}
+
+	void storeContainer(ContainerWrapper container) noexcept override
+	{
+		std::lock_guard<std::mutex> lock(m_storageMutex);
+		while (!m_storage[m_writeIndex].empty()
+			   && getReferenceCounter(m_storage[m_writeIndex]).hasUsers()) {
+			m_writeIndex = (nextIndex(m_writeIndex));
+			std::ranges::for_each(m_readIndex, [&](uint16_t& readIdx) {
+				readIdx = (nextIndex(readIdx));
+			});
+		}
+
+		m_storage[m_writeIndex].assign(container, *m_allocationBuffer);
+		m_writeIndex = (nextIndex(m_writeIndex));
+
+		std::ranges::for_each(m_readIndex, [&](uint16_t& readIdx) {
+			if (readIdx == m_writeIndex) {
+				readIdx = (nextIndex(readIdx));
+			}
+		});
+	}
+
+	std::optional<ReferenceCounterHandler<OutputContainer>>
+	getContainer(const std::size_t readerGroupIndex) noexcept override
+	{
+		std::lock_guard<std::mutex> lock(m_storageMutex);
+		if (m_readIndex[readerGroupIndex] == m_writeIndex) {
+			return std::nullopt;
+		}
+		ContainerWrapper& container = m_storage[m_readIndex[readerGroupIndex]];
+		m_readIndex[readerGroupIndex] = nextIndex(m_readIndex[readerGroupIndex]);
+		return std::make_optional<ReferenceCounterHandler<OutputContainer>>(
+			getReferenceCounter(container));
+	}
+
+	bool finished(const std::size_t readerGroupIndex) noexcept override
+	{
+		std::lock_guard<std::mutex> lock(m_storageMutex);
+		return !writersPresent() && m_readIndex[readerGroupIndex] == m_writeIndex;
+	}
+
+protected:
+	std::mutex m_storageMutex;
+	std::vector<uint16_t> m_readIndex;
+	uint16_t m_writeIndex {0};
+};
+
+} // namespace ipxp::output
---
+++ include/ipfixprobe/outputPlugin/outputStorage/serializedOutputStorageBlocking.hpp
@@ -0,0 +1,68 @@
+#pragma once
+
+#include "outputStorage.hpp"
+#include "serializedOutputStorage.hpp"
+
+#include <algorithm>
+#include <atomic>
+#include <condition_variable>
+#include <functional>
+#include <memory>
+#include <mutex>
+#include <optional>
+#include <ranges>
+#include <vector>
+
+namespace ipxp::output {
+
+class SerializedOutputStorageBlocking : public SerializedOutputStorage {
+public:
+	explicit SerializedOutputStorageBlocking(const uint8_t writersCount) noexcept
+		: SerializedOutputStorage(writersCount)
+	{
+	}
+
+	void storeContainer(ContainerWrapper container) noexcept override
+	{
+		std::unique_lock<std::mutex> lock(m_storageMutex);
+		m_storage[m_writeIndex].assign(container, *m_allocationBuffer);
+
+		m_tailNotifier.wait(lock, [&]() {
+			return !someReaderReadsNextContainerNow() && allReadersHaveReadNextContainer();
+		});
+
+		m_writeIndex = nextIndex(m_writeIndex);
+	}
+
+	std::optional<ReferenceCounterHandler<OutputContainer>>
+	getContainer(const std::size_t readerGroupIndex) noexcept override
+	{
+		std::lock_guard<std::mutex> lock(m_storageMutex);
+		m_tailNotifier.notify_all();
+		if (m_readIndex[readerGroupIndex] == m_writeIndex) {
+			return std::nullopt;
+		}
+		auto& containerPtr = m_storage[m_readIndex[readerGroupIndex]];
+		m_readIndex[readerGroupIndex] = nextIndex(m_readIndex[readerGroupIndex]);
+		return std::make_optional<ReferenceCounterHandler<OutputContainer>>(
+			getReferenceCounter(containerPtr));
+	}
+
+private:
+	bool allReadersHaveReadNextContainer() noexcept
+	{
+		return std::ranges::all_of(m_readIndex, [&](const uint16_t readIdx) {
+			return readIdx != nextIndex(m_writeIndex);
+		});
+	}
+
+	bool someReaderReadsNextContainerNow() noexcept
+	{
+		return !m_storage[nextIndex(m_writeIndex)].empty()
+			&& getReferenceCounter(m_storage[nextIndex(m_writeIndex)]).hasUsers();
+	}
+
+	std::condition_variable m_tailNotifier;
+};
+
+} // namespace ipxp::output
