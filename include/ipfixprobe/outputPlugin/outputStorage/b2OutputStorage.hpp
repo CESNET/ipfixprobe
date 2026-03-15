7f7acad97d44237bd63c3a0bfddc0df4de6afe62
Zainullin Damir <zadamsa@Xavier.>
2026-03-15 23:27:13 +0100

    ++

--- include/ipfixprobe/outputPlugin/outputStorage/b2OutputStorage.hpp
+++ include/ipfixprobe/outputPlugin/outputStorage/b2OutputStorage.hpp
@@ -113,8 +113,9 @@ public:
 			= this->m_readersData[readerIndex].get();
 		if (readerData.bucketAllocation.containersLeft()) {
 			__builtin_prefetch(
-				&readerData.bucketAllocation.bucketIndex * this->BUCKET_SIZE
-					+ readerData.bucketAllocation.containerIndex + 1,
+				&this->m_storage
+					 [readerData.bucketAllocation.bucketIndex * this->BUCKET_SIZE
+					  + readerData.bucketAllocation.containerIndex + 1],
 				PrefetchMode::Read,
 				Locality::High);
 			return &this->getNextElement(readerData.bucketAllocation).getData();
